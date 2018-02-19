d99364a4c45fae55a937ef238308943e4084e2ac
goz <goz@lab104-26.iit.uni-miskolc.hu>
2018-02-19 16:29:09 +0100

    Leibniz

--- 1hetpierteke/src/piertek.java
+++ 1hetpierteke/src/piertek.java
@@ -12,6 +12,24 @@ public class piertek {
 		System.out.println("Wallis-formula");
 		System.out.println("pi/2 erteke: " +pi);
 		System.out.println("pi erteke: "+pi*2);
+		
+		//Leibniz-fele sor
+		pi = 0;
+		darab *=4;
+		int elojel = 1;
+		
+		for (int i = 1; i <= darab; i+=2) {
+			pi += elojel * 1.0 / i;
+			elojel *= -1;
+			
+			}
+		
+		System.out.println("Leibniz-fele sor");
+		System.out.println("pi/4 erteke: " +pi);
+		System.out.println("pi erteke: "+pi*4);
+		
+		//Faktorialis
+		
 	}
 	
 
