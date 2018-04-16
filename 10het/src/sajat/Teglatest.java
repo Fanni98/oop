8e61ff741709d8f8bdbcd976364499db72b3cf5f
goz <goz@lab104-26.iit.uni-miskolc.hu>
2018-04-16 16:40:12 +0200

    gyak

---
+++ 10het/src/sajat/Teglatest.java
@@ -0,0 +1,30 @@
+package sajat;
+
+public class Teglatest extends Hasab {
+
+		private double a;
+		private double b;
+		
+		
+	public Teglatest(double magassag, double a, double b) {
+			super(magassag);
+			this.a = a;
+			this.b = b;
+		}
+
+
+	@Override
+	public double getAlapterulet() {
+		return a * b;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Teglatest [a=" + a + ", b=" + b + ",Magassag()="
+				+ getMagassag() + "]";
+	}
+	
+	
+
+}
